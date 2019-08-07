3e4ff57c9e22fd19304fe3f0f035234f538dfb42
adai <adai_mail@126.com>
2019-08-07 11:47:47 +0800

    fix

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+configs = {
+    'debug' : True,
+    'db': {
+        'host': '172.16.180.129',
+        'port': 3306,
+        'user': 'ops',
+        'password': '123456',
+        'db': 'ops'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+Override configurations.
+"""
+
+
+configs = {
+    'db': {
+        'host': '172.16.180.129'
+    }
+}
